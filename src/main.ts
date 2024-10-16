4d5efe2a907d626d7ef9d93bc24a9b8df667223f
TijanAyo <ayomidetijani6@gmail.com>
2024-10-16 17:54:32 +0100

    initializing the resources into the main class

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,25 @@ import "reflect-metadata";
 
 import axios, { AxiosInstance } from "axios";
 import { injectable } from "tsyringe";
+import { LendSqrAPIError } from "./utils";
+import {
+  CreditBureaus,
+  DataForLenders,
+  Decisioning,
+  DirectDebit,
+  EmbeddedLoansAndPayments,
+  Validation,
+} from "./resources";
 
 @injectable()
 export class Lendsqr {
   private axiosInstance: AxiosInstance;
+  public creditBureaus: CreditBureaus;
+  public dataForLenders: DataForLenders;
+  public decisioning: Decisioning;
+  public directDebit: DirectDebit;
+  public embeddedLoansAndPayment: EmbeddedLoansAndPayments;
+  public validation: Validation;
 
   constructor(
     private token: string,
@@ -18,6 +33,13 @@ export class Lendsqr {
         "Content-Type": "application/json",
       },
     });
+
+    this.creditBureaus = new CreditBureaus(this);
+    this.dataForLenders = new DataForLenders(this);
+    this.decisioning = new Decisioning(this);
+    this.directDebit = new DirectDebit(this);
+    this.embeddedLoansAndPayment = new EmbeddedLoansAndPayments(this);
+    this.validation = new Validation(this);
   }
 
   async apiRequest<T>(
@@ -31,7 +53,7 @@ export class Lendsqr {
       const response = await this.axiosInstance.request<T>(config);
       return response.data;
     } catch (error) {
-      throw error;
+      throw new LendSqrAPIError(error);
     }
   }
 }
